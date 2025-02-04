e522a0666e28f440f61c3fbb0c8609c145b813b6
Muhammad Qasim <dev.mhdqasim@gmail.com>
2025-02-04 06:29:49 +0500

    industries imgs dynamic on tab change

--- components/IndustriesPitch/index.jsx
+++ components/IndustriesPitch/index.jsx
@@ -5,7 +5,17 @@ import { IndustryWrapper } from '../../components/Section';
 
 const Index = () => {
     const [selectedTab, setSelectedTab] = useState('Customized Automations')
-    const [selectedIndustriesTab, setSelectedIndustriesTab] = useState('Education')
+    const [selectedIndustriesTab, setSelectedIndustriesTab] = useState({ title: "Property Management", imgUrl: "/Property-Managment.png" })
+
+    const industryItems = [
+        { title: "Logistics", imgUrl: '/Logistics.png' },
+        { title: "Manufacturing", imgUrl: "/Manufacutring.png" },
+        { title: "Education", imgUrl: "/Education.png" },
+        { title: "Coaching", imgUrl: "/Coach.png" },
+        { title: "Property Management", imgUrl: "/Property-Managment.png" },
+        { title: "Eco Travel", imgUrl: "/Eco-Travel.png" },
+        { title: "Concierge Services", imgUrl: "/Conceirage.png" }
+        ]
 
     return (
         <>
@@ -61,19 +71,19 @@ const Index = () => {
             >
                 <div className='mb-30'>
                     <section className='flex items-stretch gap-16 flex-wrap xl:flex-nowrap'>
-                        {['Logistics', 'Manufacturing', 'Education', 'Coaching', 'Property Management', 'Eco Travel', 'Concierge Services']?.map((item, _index) => (
-                            <Tab key={_index} onClick={() => setSelectedIndustriesTab(item)} className={`${item == selectedIndustriesTab && '!bg-[#E95018] !border-white'} group border border-[#6F6F6F] hover:!bg-[#E95018] hover:!border-white rounded-none`}>
-                                <span className={`${item == selectedIndustriesTab && '!text-white'} text-[#6F6F6F] group-hover:text-white font-semibold text-sm xl:text-base`}>{item}</span>
+                        {industryItems?.map((item, _index) => (
+                            <Tab key={_index} onClick={() => setSelectedIndustriesTab(item)} className={`${item?.title == selectedIndustriesTab?.title && '!bg-[#E95018] !border-white'} group border border-[#6F6F6F] hover:!bg-[#E95018] hover:!border-white rounded-none`}>
+                                <span className={`${item?.title == selectedIndustriesTab?.title && '!text-white'} text-[#6F6F6F] group-hover:text-white font-semibold text-sm xl:text-base`}>{item?.title}</span>
                             </Tab>
                         ))}
                     </section>
                 </div>
                 <section className='bg-[#121212] flex flex-col lg:flex-row items-center justify-between gap-36 md:gap-99 px-24 md:px-72 pt-36 pb-70'>
                     <div>
-                        <H3 className={'!font-semibold mb-24'}>{selectedIndustriesTab}</H3>
-                        <H6>Streamline your logistics operations with our AI-powered automation solutions. We specialize in optimizing supply chains, reducing costs, and improving efficiency.</H6>
+                        <H3 className={'!font-semibold mb-24'}>{selectedIndustriesTab?.title}</H3>
+                        <H6>Streamline your <span className='lowercase'>{selectedIndustriesTab?.title}</span> operations with our AI-powered automation solutions. We specialize in optimizing supply chains, reducing costs, and improving efficiency.</H6>
                     </div>
-                    <img className='w-full md:w-full object-none px-20 md:pr-0' src="/assets/logistics-graphic.svg" alt="" />
+                    <img className='w-full md:w-full max-h-[360px] object-contain px-20 md:pr-0' src={`/assets/globe${selectedIndustriesTab?.imgUrl}`} alt="" />
                 </section>
             </IndustryWrapper>
 
